8bce0827ff124fa98a26803ef153203ffcf38232
VIPsth <VIPsongtaohong@yeah.net>
2019-01-14 17:50:39 +0800

    add

--- static/js/custom1.js
+++ static/js/custom1.js
@@ -65,7 +65,7 @@ function getydxc() {
 
 				var title = "金西亚驾校服务平台";
 				// var link = "http://ydxctrue.yidianxueche.cn/";
-				var link = "http://ydxc.yidianxueche.cn/";
+				var link = "http://jxyjx.yidianxueche.cn/";
 				var imgUrl = "http://ydxctrue.yidianxueche.cn/template/wap/public/css/self/image/banner_1.jpg";
 				var desc = "金西亚驾校，让学车变得容易点";
 				var type = "";
